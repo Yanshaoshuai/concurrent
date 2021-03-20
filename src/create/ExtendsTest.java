ff012cb846ec0bf82693f38606c77c3064e5cbb8
yanshaoshuai <ahutyss@gmail.com>
2021-03-20 13:54:13 +0800

    init

---
+++ src/create/ExtendsTest.java
@@ -0,0 +1,30 @@
+package create;
+
+import javafx.scene.Parent;
+
+/**
+ * @Author YSS
+ * @Date 2020/6/11 23:26
+ */
+public class ExtendsTest {
+    public static class Parent {
+        public void run() {
+            System.out.println("Parent#run");
+        }
+
+        public void start() {
+            this.run();
+        }
+    }
+    static class  Child  extends Parent{
+        @Override
+        public void run() {
+            System.out.println("Child#run");
+        }
+    }
+
+    public static void main(String[] args) {
+        Parent parent=new Child();
+        parent.start();
+    }
+}
---
+++ src/singleton/TestSingleton8.java
@@ -0,0 +1,11 @@
+package singleton;
+
+/**
+ * @Author YSS
+ * @Date 2020/6/14 19:01
+ */
+public class TestSingleton8 {
+    public static void main(String[] args) {
+        Singleton8.INSTANCE.whatever();
+    }
+}
